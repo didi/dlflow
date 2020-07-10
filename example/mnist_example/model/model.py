835e406b7ad327a8c775a6a1efb6bc38d5925e8f
zhouyc <yuanchenzhouhcmy@gmail.com>
2020-07-10 21:42:20 +0800

    Update model.py

--- example/mnist_example/model/model.py
+++ example/mnist_example/model/model.py
@@ -1,20 +1,19 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense,Conv2D,BatchNormalization, MaxPooling2D
-from tensorflow.keras.layers import ReLU,Dropout,Flatten
+from tensorflow.keras.layers import Dense, Conv2D
+from tensorflow.keras.layers import BatchNormalization, MaxPooling2D
+from tensorflow.keras.layers import ReLU, Dropout, Flatten
 
 
 class CNN(Model):
-    """
-    """
-    def __init__(self,n_class=10):
+    def __init__(self, n_class=10):
         super(CNN, self).__init__()
 
-        self.conv1 = Conv2D(32,(3,3),activation='relu')
-        self.conv2 = Conv2D(64,(3,3),activation='relu')
+        self.conv1 = Conv2D(32, (3, 3), activation='relu')
+        self.conv2 = Conv2D(64, (3, 3), activation='relu')
         self.max_pooing2d = MaxPooling2D((2, 2))
         self.flatten = Flatten()
-        self.dense1 = Dense(64,activation='relu')
-        self.dense2 = Dense(n_class,activation='softmax')
+        self.dense1 = Dense(64, activation='relu')
+        self.dense2 = Dense(n_class, activation='softmax')
 
     def call(self, inputs):
         x = self.conv1(inputs)
@@ -24,4 +23,5 @@ class CNN(Model):
         x = self.flatten(x)
         x = self.dense1(x)
         x = self.dense2(x)
+        
         return x
